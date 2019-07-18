a38952bda647c15e5e6d51659f907f1b76c05fae
SaraEbrahim <sara.elkafrawy@gmail.com>
2019-07-18 04:31:37 -0400

    Updating server.py with cassava model and classes

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_name = 'export.pkl'
+export_file_url = 'https://drive.google.com/uc?export=download&id=12joT5I5OHb_KdQAYO78PtLdfkSAZWtIa'
+export_file_name = 'cassava_best_model.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['cbb','cbsd','cgm','cmd','healthy']
 path = Path(__file__).parent
 
 app = Starlette()
