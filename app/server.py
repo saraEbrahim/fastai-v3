3d83f2cd575784d27f181f149729a84a69fd0338
SaraEbrahim <sara.elkafrawy@gmail.com>
2019-07-18 05:25:14 -0400

    fix downloadable url instead of sharable

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://drive.google.com/file/d/1nDjUBjQto8Bd4XIFcy1gp2yvGq66nKZn/view?usp=sharing'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1nDjUBjQto8Bd4XIFcy1gp2yvGq66nKZn'
 export_file_name = 'cassava_best_model.pkl'
 
 classes = ['cbb','cbsd','cgm','cmd','healthy']
